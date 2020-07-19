04739b3c43eb4e344aa6f02a43c216f5a82c0cd7
Lucas Carrias <lucascarrias@outlook.com>
2020-07-19 16:55:36 -0300

    Made Flashligth and Battery service

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 import bobo
+from http import HTTPStatus
+
 
 class Flashligth():
     def __init__(self):
         self.is_on = False
 
-    
     def set_on(self, turn_on):
         self.is_on= turn_on
 
     def toggle(self):
         self.is_on = not self.is_on
 
+    def get_status(self):
+        return {
+            "isOn":self.is_on
+        }
+
 class Battery():
-    STATUS= ["ok", "charging", "low", "unknown"]
+    MODES = ["normal", "charging", "unknown"]
     def __init__(self):
-        self.power = None
-        self.current_status = self.STATUS[-1]
+        self.power = ""
+        self.mode = self.MODES[-1]
+
+    def set_mode(self, mode_index):
+        self.mode = self.MODES[mode_index]
+
+    def set_power(self, value):
+        self.power = value
+
+    def get_status(self):
+        return {
+            "mode": self.mode,
+            "power": self.power
+        }
 
 class Cellphone():
     def __init__(self):
@@ -31,21 +49,34 @@ def flahslight_get():
         "isOn": cellphone.flashlight.is_on
     }
 
+
 @bobo.post('/flashlight', content_type='application/json')
 def flahslight_post(turn_on):
-    print(turn_on)
     if not type(turn_on) is bool:
-        raise RuntimeError("The value must be a bool")
-
+        raise RuntimeError(HTTPStatus(400))
     cellphone.flashlight.set_on(turn_on)
-
     return {
         "isOn": cellphone.flashlight.is_on
     }
 
-@bobo.get('/battery', content_type='application/json')
-def battery():
-    return {
-        "status": cellphone.battery.current_status,
-        "power": cellphone.battery.power
-    }
+
+@bobo.get('/battery/:info', content_type='application/json')
+def battery(info=None):
+    if info == "power":
+        return cellphone.battery.power
+    elif info == "mode":
+        return cellphone.battery.mode
+    return cellphone.battery.get_status()
+
+
+@bobo.post('/battery/power', content_type='application/json')
+def battery_power_post(value):
+    cellphone.battery.set_power(value)
+    return cellphone.battery.get_status()
+
+
+@bobo.post('/battery/mode', content_type='application/json')
+def battery_mode_post(value):
+    cellphone.battery.set_mode(value)
+    return cellphone.battery.get_status()
+
